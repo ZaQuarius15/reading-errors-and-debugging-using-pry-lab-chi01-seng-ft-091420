f5a85ab90d7da4bde3bc2f0eded54d0ab27c53b3
Zack Carlson <jzcarlson014@gmail.com>
2020-08-14 02:36:30 +0000

    Automatically backed up by Learn

--- fix_using_tests/pemdos.rb
+++ fix_using_tests/pemdos.rb
@@ -2,10 +2,9 @@ require 'pry' # don't forget to add: require 'pry'
 
 def snake_it_up(string)
   if string[0] == "s"
-    10.times do
-      string = "s" + string
-    end
-    string
+    10 * "s" + string
+    binding.pry
+    puts "pry"
   else
     string
   end
